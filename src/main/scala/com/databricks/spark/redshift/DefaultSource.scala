7c7e124c8ce47b18343ae2f32884a478df969607
James Aley <james.aley@gmail.com>
2015-07-13 13:54:51 +0100

    Implement CreatableRelationProvider for save functionality

--- src/main/scala/com/databricks/spark/redshift/DefaultSource.scala
+++ src/main/scala/com/databricks/spark/redshift/DefaultSource.scala
@@ -1,7 +1,11 @@
 package com.databricks.spark.redshift
 
-import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.sources.{BaseRelation, RelationProvider, SchemaRelationProvider}
+import java.util.Properties
+
+import org.apache.spark.Logging
+import org.apache.spark.sql.jdbc.RedshiftJDBCWrapper
+import org.apache.spark.sql.{DataFrame, SaveMode, SQLContext}
+import org.apache.spark.sql.sources.{CreatableRelationProvider, BaseRelation, RelationProvider, SchemaRelationProvider}
 import org.apache.spark.sql.types.StructType
 
 /**
@@ -9,7 +13,9 @@ import org.apache.spark.sql.types.StructType
  */
 class DefaultSource
   extends RelationProvider
-  with SchemaRelationProvider {
+  with SchemaRelationProvider
+  with CreatableRelationProvider
+  with Logging {
 
   def checkTempPath(params: Map[String, String]) = {
     params.getOrElse("tempPath", sys.error("'tempPath' is required for all Redshift loads and saves"))
@@ -36,5 +42,40 @@ class DefaultSource
    */
   override def createRelation(sqlContext: SQLContext, parameters: Map[String, String], schema: StructType): BaseRelation = {
     RedshiftRelation(checkTable(parameters), checkUrl(parameters), checkTempPath(parameters), Some(schema))(sqlContext)
+  }
+
+  /**
+   * Creates a Relation instance by first writing the contents of the given DataFrame to Redshift
+   */
+  override def createRelation(sqlContext: SQLContext, mode: SaveMode, parameters: Map[String, String], data: DataFrame): BaseRelation = {
+    val url = checkUrl(parameters)
+    val tempPath = checkTempPath(parameters)
+    val table = checkTable(parameters)
+    val getConnection = RedshiftJDBCWrapper.getConnector(PostgresDriver.CLASS_NAME, url, new Properties())
+    val conn = getConnection()
+
+    val (doSave, dropExisting) = mode match {
+      case SaveMode.Append => (true, false)
+      case SaveMode.Overwrite => (true, true)
+      case SaveMode.ErrorIfExists =>
+        if(RedshiftJDBCWrapper.tableExists(conn, table)) {
+          sys.error(s"Table $table already exists! (SaveMode is set to ErrorIfExists)")
+        } else {
+          (true, false)
+        }
+      case SaveMode.Ignore =>
+        if(RedshiftJDBCWrapper.tableExists(conn, table)) {
+          log.info(s"Table $table already exists -- ignoring save request.")
+          (false, false)
+        } else {
+          (true, false)
+        }
+    }
+
+    if(doSave) {
+      RedshiftWriter.saveToRedshift(data, url, table, tempPath, dropExisting, getConnection)
+    }
+
+    createRelation(sqlContext, parameters)
   }
 }
--- src/main/scala/org/apache/spark/sql/jdbc/RedshiftJDBCWrapper.scala
+++ src/main/scala/org/apache/spark/sql/jdbc/RedshiftJDBCWrapper.scala
@@ -1,9 +1,13 @@
 package org.apache.spark.sql.jdbc
 
+import java.sql.Connection
 import java.util.Properties
 
 import org.apache.spark.sql.DataFrame
 
+/**
+ * Hack to access some private JDBC SQL functionality.
+ */
 object RedshiftJDBCWrapper {
   def schemaString(dataFrame: DataFrame, url: String) = JDBCWriteDetails.schemaString(dataFrame, url)
   def registerDriver(driverClass: String) = DriverRegistry.register(driverClass)
@@ -11,4 +15,5 @@ object RedshiftJDBCWrapper {
     JDBCRDD.resolveTable(jdbcUrl, table, properties)
   def getConnector(driver: String, url: String, properties: Properties) =
     JDBCRDD.getConnector(driver, url, properties)
+  def tableExists(conn: Connection, table: String) = JdbcUtils.tableExists(conn, table)
 }
