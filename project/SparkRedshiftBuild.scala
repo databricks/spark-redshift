b479bb10d91c765de52e8cb10be23f7d366e651b
Josh Rosen <joshrosen@databricks.com>
2015-08-18 11:47:40 -0700

    Move from .sbt to .scala SBT build definition.

---
+++ project/SparkRedshiftBuild.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015 Databricks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import sbt._
+import sbt.Keys._
+import sbtsparkpackage.SparkPackagePlugin.autoImport._
+import scoverage.ScoverageSbtPlugin
+
+object SparkRedshiftBuild extends Build {
+  val hadoopVersion = settingKey[String]("Hadoop version")
+
+  lazy val root = Project("spark-redshift", file("."))
+    .configs(IntegrationTest)
+    .settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*)
+    .settings(Defaults.itSettings: _*)
+    .settings(Seq(
+      name := "spark-redshift",
+      organization := "com.databricks",
+      version := "0.4.1-SNAPSHOT",
+      scalaVersion := "2.10.4",
+      sparkVersion := sys.props.get("spark.version").getOrElse("1.4.1"),
+      hadoopVersion := sys.props.get("hadoop.version").getOrElse("2.2.0"),
+      spName := "databricks/spark-redshift",
+      sparkComponents += "sql",
+      licenses += "Apache-2.0" -> url("http://opensource.org/licenses/Apache-2.0"),
+      credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+      resolvers +=
+        "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      libraryDependencies ++= Seq(
+        "com.amazonaws" % "aws-java-sdk-core" % "1.9.40" % "provided",
+        // We require spark-avro, but avro-mapred must be provided to match Hadoop version,
+        "com.databricks" %% "spark-avro" % "1.0.0",
+        "org.apache.avro" % "avro-mapred" % "1.7.6" % "provided" exclude("org.mortbay.jetty", "servlet-api"),
+        // A Redshift-compatible JDBC driver must be present on the classpath for spark-redshift to work.
+        // For testing, we using a Postgres driver, but it is recommended that the Amazon driver is used
+        // in production. See http://docs.aws.amazon.com/redshift/latest/mgmt/configure-jdbc-connection.html
+        "postgresql" % "postgresql" % "8.3-606.jdbc4" % "provided",
+        "com.google.guava" % "guava" % "14.0.1" % Test,
+        "org.scalatest" %% "scalatest" % "2.1.5" % Test,
+        "org.scalamock" %% "scalamock-scalatest-support" % "3.2" % Test
+      ),
+      ScoverageSbtPlugin.ScoverageKeys.coverageHighlighting := {
+        if (scalaBinaryVersion.value == "2.10") false
+        else false
+      }
+    ): _*)
+}
