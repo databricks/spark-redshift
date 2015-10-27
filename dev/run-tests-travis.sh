2fee7799bd12780f3b8ba08d5e2ef68f5d336da8
Josh Rosen <joshrosen@databricks.com>
2015-10-27 14:24:43 -0700

    Fix Hadoop and Spark version configurations in integration tests
    This patch fixes an issue where incorrect system properties were being used to specify the Spark and Hadoop versions during integration tests (the unit tests were using the correct properties).
    
    Luckily I did not discover any bugs that would indicate broken Spark / Hadoop compatibility, although I did run into a few integration test configurations that needed to be updated in order to properly handle overridden dependencies.
    
    Author: Josh Rosen <joshrosen@databricks.com>
    
    Closes #112 from JoshRosen/fix-hadoop-version-in-integration-tests.

---
+++ dev/run-tests-travis.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+sbt ++$TRAVIS_SCALA_VERSION scalastyle
+sbt ++$TRAVIS_SCALA_VERSION "test:scalastyle"
+sbt ++$TRAVIS_SCALA_VERSION "it:scalastyle"
+
+sbt \
+  -Dhadoop.testVersion=$HADOOP_VERSION \
+  -Dspark.testVersion=$SPARK_VERSION \
+  ++$TRAVIS_SCALA_VERSION \
+  coverage test
+
+if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+  sbt \
+    -Dhadoop.testVersion=$HADOOP_VERSION \
+    -Dspark.testVersion=$SPARK_VERSION \
+    ++$TRAVIS_SCALA_VERSION \
+    coverage it:test 2> /dev/null;
+fi
