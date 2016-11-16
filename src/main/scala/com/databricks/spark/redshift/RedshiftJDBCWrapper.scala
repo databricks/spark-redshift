9052d67de7a4956c5540e11dcd1b88bc8a3ca997
Josh Rosen <joshrosen@databricks.com>
2016-11-16 13:17:57 -0800

    Fix by querying RSMD of prepared statement rather than resultset.

--- src/main/scala/com/databricks/spark/redshift/RedshiftJDBCWrapper.scala
+++ src/main/scala/com/databricks/spark/redshift/RedshiftJDBCWrapper.scala
@@ -151,9 +151,9 @@ private[redshift] class JDBCWrapper {
    * @throws SQLException if the table contains an unsupported type.
    */
   def resolveTable(conn: Connection, table: String): StructType = {
-    val rs = executeQueryInterruptibly(conn.prepareStatement(s"SELECT * FROM $table WHERE 1=0"))
+    val ps = conn.prepareStatement(s"SELECT * FROM $table WHERE 1=0")
     try {
-      val rsmd = rs.getMetaData
+      val rsmd = executeInterruptibly(ps, _.getMetaData)
       val ncols = rsmd.getColumnCount
       val fields = new Array[StructField](ncols)
       var i = 0
@@ -170,7 +170,7 @@ private[redshift] class JDBCWrapper {
       }
       new StructType(fields)
     } finally {
-      rs.close()
+      ps.close()
     }
   }
 
@@ -267,7 +267,8 @@ private[redshift] class JDBCWrapper {
     // Somewhat hacky, but there isn't a good way to identify whether a table exists for all
     // SQL database systems, considering "table" could also include the database name.
     Try {
-      executeQueryInterruptibly(conn.prepareStatement(s"SELECT 1 FROM $table LIMIT 1")).next()
+      val stmt = conn.prepareStatement(s"SELECT 1 FROM $table LIMIT 1")
+      executeInterruptibly(stmt, _.getMetaData).getColumnCount
     }.isSuccess
   }
 
