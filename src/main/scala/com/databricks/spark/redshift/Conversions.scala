475e7a1a138142da73fc6ec5c012aa5300b06435
Francesco Di Chiara <fdc@yelp.com>
2018-05-11 18:12:15 -0700

    Fix convertion bit and test

--- src/main/scala/com/databricks/spark/redshift/Conversions.scala
+++ src/main/scala/com/databricks/spark/redshift/Conversions.scala
@@ -116,7 +116,7 @@ private[redshift] object Conversions {
       var i = 0
       while (i < schema.length) {
         val data = inputRow(i)
-        converted(i) = if (data == null || data == nullString) null else if (data.isEmpty) "" else conversionFunctions(i)(data)
+        converted(i) = if (data == null || data == nullString || (data.isEmpty && schema.fields(i).dataType != StringType)) null else if (data.isEmpty) "" else conversionFunctions(i)(data)
         i += 1
       }
       encoder.toRow(externalRow)
