9cd9c781a4dcee6d977426e8d6af8185edbb20ad
Ricardo Fanjul <ricardo.fanjul@letgo.com>
2018-08-20 12:34:40 +0200

    - Letgo release. - Fixed error between S3Guard and Spark Redshift.

--- src/main/scala/com/databricks/spark/redshift/RedshiftWriter.scala
+++ src/main/scala/com/databricks/spark/redshift/RedshiftWriter.scala
@@ -314,12 +314,16 @@ private[redshift] class RedshiftWriter(
       // S3, so let's first sanitize `tempdir` and make sure that it uses the s3:// scheme:
       val sanitizedTempDir = Utils.fixS3Url(
         Utils.removeCredentialsFromURI(URI.create(tempDir)).toString).stripSuffix("/")
+      val sanitizedTempDirS3GuardCompatible =
+        Utils.removeCredentialsFromURI(URI.create(tempDir)).toString.stripSuffix("/")
+
       val manifestEntries = filesToLoad.map { file =>
         s"""{"url":"$sanitizedTempDir/$file", "mandatory":true}"""
       }
       val manifest = s"""{"entries": [${manifestEntries.mkString(",\n")}]}"""
       val manifestPath = sanitizedTempDir + "/manifest.json"
-      val fsDataOut = fs.create(new Path(manifestPath))
+      val manifestPathS3GuardCompatible = sanitizedTempDirS3GuardCompatible + "/manifest.json"
+      val fsDataOut = fs.create(new Path(sanitizedTempDirS3GuardCompatible))
       try {
         fsDataOut.write(manifest.getBytes("utf-8"))
       } finally {
